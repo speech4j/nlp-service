19411f495deff31b536a06db94b883be8742fd4c
Oleh Onunfryk <olehonufryk@gmail.com>
2020-04-16 17:59:32 +0300

    Added test endpoint

---
+++ src/test/java/org/speech4j/nlpservice/controller/IndexControllerTest.java
@@ -0,0 +1,14 @@
+package org.speech4j.nlpservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
