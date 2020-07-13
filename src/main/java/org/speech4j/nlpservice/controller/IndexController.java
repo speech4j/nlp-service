1ddc6c2ac9670e19ae1f4c5031dc1fb77d30835e
Oleh Onunfryk <olehonufryk@gmail.com>
2020-07-13 23:42:01 +0300

    added aws sdk for comprehend service

--- src/main/java/org/speech4j/nlpservice/controller/IndexController.java
+++ src/main/java/org/speech4j/nlpservice/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping
-    public String hello(){
+    public String hello() {
         return "Hello";
     }
 }
